ac64a13b8a9aca5759aee19060e936cc1ee269e7
fitocs452 <fitomm@hotmail.es>
2016-05-09 19:31:37 -0600

    Últimas modificaciones a los queries

--- Queries/client.sql
+++ Queries/client.sql
@@ -1,17 +1,18 @@
 CREATE SEQUENCE client_id_seq INCREMENT BY 1 MINVALUE 1 START 1;
 CREATE TABLE client (
     id INT NOT NULL,
-    fechaNacimiento VARCHAR(255) DEFAULT NULL,
+    fecha_nacimiento VARCHAR(255) DEFAULT NULL,
     nit VARCHAR(10) NOT NULL,
     frecuente BOOLEAN DEFAULT NULL,
     nombres VARCHAR(255) NOT NULL,
     apellidos VARCHAR(255) DEFAULT NULL,
-    estadoCivil VARCHAR(255) NOT NULL,
-    fotoCliente VARCHAR(255) DEFAULT NULL,
+    estado_civil VARCHAR(255) NOT NULL,
+    foto_cliente VARCHAR(255) DEFAULT NULL,
     sexo VARCHAR(255) DEFAULT NULL,
     profesion VARCHAR(255) DEFAULT NULL,
     dpi VARCHAR(14) DEFAULT NULL,
     nacionalidad VARCHAR(255) DEFAULT NULL,
+    twitter_username VARCHAR(25) DEFAULT NULL,
     PRIMARY KEY(id)
 );
 
@@ -19,8 +20,8 @@ ALTER TABLE client
     ADD tipo_membresia_id INT DEFAULT NULL;
 
 ALTER TABLE client
-    ADD CONSTRAINT FK_C74404553D81FAA9
+    ADD CONSTRAINT FK_TIPO_MEMBRESIA
     FOREIGN KEY (tipo_membresia_id)
-    REFERENCES tipo_membresia (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
+    REFERENCES tipo_membresia (id);
 
-CREATE INDEX IDX_C74404553D81FAA9 ON client (tipo_membresia_id);
+CREATE INDEX IDX_TIPO_MEMBRESIA ON client (tipo_membresia_id);
