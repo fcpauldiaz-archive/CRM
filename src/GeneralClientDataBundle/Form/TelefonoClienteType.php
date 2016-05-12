2507db3443724414b459586d7da50d1b86d1f4f4
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2016-05-11 19:06:21 -0600

    insert cliente

---
+++ src/GeneralClientDataBundle/Form/TelefonoClienteType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace GeneralClientDataBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+
+class TelefonoClienteType extends AbstractType
+{
+   
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('numeroTelefono', 'text', [
+            'label' => 'Número de Telefono',
+            'required' => true,
+        ])
+      
+
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'telefono';
+    }
+}
